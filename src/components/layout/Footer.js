af2d2ab8d2c73ad290cfb30d4349c66d1f207aec
Mama africa <k_vashalomidze@mail.ru>
2025-02-20 14:56:42 +0400

    fixed footer abrupting ui

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { personalInfo } from '../../data/personal';
 
 const FooterContainer = styled.footer`
   background-color: ${props => props.theme.colors.primary};
@@ -13,70 +12,23 @@ const FooterContainer = styled.footer`
 const FooterContent = styled.div`
   max-width: 800px;
   margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: ${props => props.theme.spacing.md};
-`;
-
-const SocialLinks = styled.div`
-  display: flex;
-  gap: ${props => props.theme.spacing.lg};
-  margin-bottom: ${props => props.theme.spacing.md};
-`;
-
-const SocialLink = styled.a`
-  color: white;
-  font-family: ${props => props.theme.fonts.main};
-  font-size: ${props => props.theme.fontSizes.md};
-  text-decoration: none;
-  padding: ${props => props.theme.spacing.xs};
-  
-  &:hover {
-    color: ${props => props.theme.colors.accent};
-    text-decoration: underline;
-  }
+  text-align: center;
 `;
 
 const Copyright = styled.p`
   font-size: ${props => props.theme.fontSizes.sm};
-  text-align: center;
+  padding: ${props => props.theme.spacing.sm};
+  
+  @media (max-width: ${props => props.theme.breakpoints.mobile}) {
+    font-size: ${props => props.theme.fontSizes.xs};
+    padding: ${props => props.theme.spacing.xs};
+  }
 `;
 
 function Footer() {
   return (
     <FooterContainer>
       <FooterContent>
-        <SocialLinks>
-          <SocialLink 
-            href={personalInfo.platforms.GitHub}
-            target="_blank" 
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </SocialLink>
-          <SocialLink 
-            href={personalInfo.platforms.LinkedIn}
-            target="_blank" 
-            rel="noopener noreferrer"
-          >
-            LinkedIn
-          </SocialLink>
-          <SocialLink 
-            href={personalInfo.platforms.Leetcode}
-            target="_blank" 
-            rel="noopener noreferrer"
-          >
-            LeetCode
-          </SocialLink>
-          <SocialLink 
-            href={personalInfo.platforms.Hackerrank}
-            target="_blank" 
-            rel="noopener noreferrer"
-          >
-            HackerRank
-          </SocialLink>
-        </SocialLinks>
         <Copyright>
           © {new Date().getFullYear()} Konstantine Vashalomidze. All rights reserved.
         </Copyright>
