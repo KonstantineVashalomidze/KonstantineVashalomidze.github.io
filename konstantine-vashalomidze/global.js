49d640c01c1dd3181cbf78cf51433e5cc2f707e2
KonstantineVasahalomize <vashalomidzekonstantine@gmail.com>
2025-11-27 02:38:49 +0400

    feat: sherekilebi.mp3 instead of snow.mp3

--- konstantine-vashalomidze/global.js
+++ konstantine-vashalomidze/global.js
@@ -2,7 +2,7 @@
 const snowAudio = document.createElement("audio");
 const snowAudioPlayPauseButton = document.createElement("button");
 
-snowAudio.src = "../resources/audios/snow.mp3";
+snowAudio.src = "../resources/audios/sherekilebi.mp3";
 snowAudio.loop = true;
 
 snowAudioPlayPauseButton.textContent = "Play song";
